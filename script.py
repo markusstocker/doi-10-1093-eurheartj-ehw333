fce91349e119f6cf6be1e1d095bd126030aa44b4
Markus Stocker <markus.stocker@gmail.com>
2022-09-23 14:57:41 +0200

    added param name

--- script.py
+++ script.py
@@ -15,8 +15,8 @@ pvalue = tt.pvalue
 pvalue_ceil = ceil(pvalue * 1000) / 1000.0
 
 tp.students_ttest(
-  'Statistically significant hypothesis test with IRE binding dependent variable on failing and non-failing hearts (p<{})'.format(pvalue_ceil),
-  'http://purl.obolibrary.org/obo/GO_0030350', # the study design dependent variable
-  tp.pvalue('the p-value of the statistical hypothesis test (p<{})'.format(pvalue_ceil), tp.scalar_value_specification(pvalue)),
-  'https://github.com/markusstocker/doi-10-1093-eurheartj-ehw333/blob/main/data.csv', # the input dataset ('df' WIP)
+  label='Statistically significant hypothesis test with IRE binding dependent variable on failing and non-failing hearts (p<{})'.format(pvalue_ceil), 
+  has_dependent_variable='http://purl.obolibrary.org/obo/GO_0030350', # the study design dependent variable (iron-responsive element binding)
+  has_specified_input='https://github.com/markusstocker/doi-10-1093-eurheartj-ehw333/blob/main/data.csv', # the input dataset ('df' WIP)
+  has_specified_output=tp.pvalue('the p-value of the statistical hypothesis test (p<{})'.format(pvalue_ceil), tp.scalar_value_specification(pvalue)),
 ).serialize_to_file('article.contribution.1.json', format='json-ld')
