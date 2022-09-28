65be7f5659379fa7bfdb5c141b40fb5d5b2e28d6
Markus Stocker <markus.stocker@gmail.com>
2022-09-28 17:15:48 +0200

    removed install package

--- script.R
+++ script.R
@@ -1,8 +1,9 @@
-install.packages("orkg_0.1.0.zip")
 library(orkg)
+
 orkg <- ORKG(host="https://orkg.org/")
 orkg$templates$materialize_template(template_id = "R12002")
 tp = orkg$templates$list_templates()
+
 df = read.csv('data.csv')
 tt = t.test(df[['non.failing.heart..NF.']], 
              df[['failing.heart..F.']],
@@ -11,9 +12,9 @@ pvalue = tt$p.value
 pvalue_ceil = ceiling(pvalue * 1000) / 1000.0
 
 instance <- tp$students_ttest(
-  label=paste('Statistically significant hypothesis test with IRE binding dependent variable on failing and non-failing hearts (p<',pvalue_ceil,')'), 
+  label=paste('Statistically significant hypothesis test with IRE binding dependent variable on failing and non-failing hearts (p<',pvalue_ceil,')', sep=''), 
   has_dependent_variable='http://purl.obolibrary.org/obo/GO_0030350', # the study design dependent variable (iron-responsive element binding)
   has_specified_input='https://github.com/markusstocker/doi-10-1093-eurheartj-ehw333/blob/main/data.csv', # the input dataset ('df' WIP)
-  has_specified_output=tp$pvalue(paste('the p-value of the statistical hypothesis test (p<',pvalue_ceil,')'), tp$scalar_value_specification(pvalue)),
+  has_specified_output=tp$pvalue(paste('the p-value of the statistical hypothesis test (p<',pvalue_ceil,')', sep=''), tp$scalar_value_specification(pvalue)),
 )
 instance$serialize_to_file('article.contribution.1.json', format='json-ld')
