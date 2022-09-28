038bd2b8689a88123ad6ee929fd548964552d169
Markus Stocker <markus.stocker@gmail.com>
2022-09-28 13:27:03 +0200

    fixed scalar value specification label

--- script.py
+++ script.py
@@ -13,10 +13,13 @@ tt = ttest_ind(df['non-failing heart (NF)'],
                equal_var=False, nan_policy='omit')
 pvalue = tt.pvalue
 pvalue_ceil = ceil(pvalue * 1000) / 1000.0
+pvalue_str = '{:.16f}'.format(pvalue)
 
 tp.students_ttest(
   label='Statistically significant hypothesis test with IRE binding dependent variable on failing and non-failing hearts (p<{})'.format(pvalue_ceil), 
   has_dependent_variable='http://purl.obolibrary.org/obo/GO_0030350', # the study design dependent variable (iron-responsive element binding)
   has_specified_input='https://github.com/markusstocker/doi-10-1093-eurheartj-ehw333/blob/main/data.csv', # the input dataset ('df' WIP)
-  has_specified_output=tp.pvalue('the p-value of the statistical hypothesis test (p<{})'.format(pvalue_ceil), tp.scalar_value_specification(pvalue)),
+  has_specified_output=tp.pvalue('the p-value of the statistical hypothesis test (p<{})'.format(pvalue_ceil), 
+    tp.scalar_value_specification('{}'.format(pvalue_str), pvalue_str)
+  ),
 ).serialize_to_file('article.contribution.1.json', format='json-ld')
